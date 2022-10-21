ddade0f834d5669e06e4ba037df782b807a1ebc5
AndiiArliyandi12 <87647174+AndiiArliyandi12@users.noreply.github.com>
2022-10-22 01:47:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -80,57 +80,57 @@ const readMore = more.repeat(4001)
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ NO OWNER ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.owner = [
-['6283866737879', '𝕭𝖔𝖙', true],
-['6283866737879', 'Andii ༢࿔ྀ', true]
+['6282376267531', 'Aisyah', true],
+['6281361281833', 'Aldi', true]
 ]
 global.mods = []
 global.prems = []
-global.nomorbot = '6283866737879'
-global.nomorown = '6283866737879'
-global.nomorown1 = '6283866737879'
-global.nomorown2 = '6283866737879'
+global.nomorbot = '6283838186170'
+global.nomorown = '6281361281833'
+global.nomorown1 = '6281361281833'
+global.nomorown2 = '6282376267531'
 global.nomorwa = '0'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ WM ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.readMore = readMore
-global.author = '𝕭𝖔𝖙 • Andii ༢࿔ྀ'
-global.namebot = '​𝕭𝖔𝖙 • Andii ༢࿔ྀ
-global.wm = '© 𝘾𝙧𝙚𝙖𝙩𝙚𝙙 𝘽𝙮 𝕭𝖔𝖙 • Andii ༢࿔ྀ'
+global.author = 'Aʟᴅɪ X Aɪsʏᴀʜ'
+global.namebot = '​ 𝐘𝐮𝐬𝐡𝐢𝐧𝐨 𝐌𝐝'
+global.wm = '© 𝘾𝙧𝙚𝙖𝙩𝙚𝙙 𝘽𝙮 𝐘𝐮𝐬𝐡𝐢𝐧𝐨 𝐌𝐝'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
-global.wm = '𝕭𝖔𝖙 • Andii ༢࿔ྀ '
+global.wm = '𝒀𝒖𝒔𝒉𝒊𝒏𝒐 𝑴𝒅 '
 global.watermark = wm
-global.wm2 = '𝕭𝖔𝖙 • Andii ༢࿔ྀ '
+global.wm2 = '𝒀𝒖𝒔𝒉𝒊𝒏𝒐 𝑴𝒅 '
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `𝕭𝖔𝖙 • Andii ༢࿔ྀ`
-global.stickpack = '𝕭𝖔𝖙 • Andii ༢࿔ྀ'
-global.stickauth = `© 𝕭𝖔𝖙 • Andii ༢࿔ྀ`
+global.titlebot = `𝑨𝒍𝒅𝒊 𝐗 𝑨𝒊𝒔𝒚𝒂𝒉`
+global.stickpack = '𝑨𝒍𝒅𝒊 𝐗 𝑨𝒊𝒔𝒚𝒂𝒉'
+global.stickauth = `© Yushino MD`
 global.week = `${week} ${date}`
 global.wibb = `${wktuwib}`
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ OWNER INFO ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.nameown1= '𝕭𝖔𝖙'
-global.nameown2 = 'Andii ༢࿔ྀ'
+global.nameown1= 'Aldi'
+global.nameown2 = 'Aisyah'
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ MEDIA LINK ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 global.Linkgc = 'Bentar'
 global.lynk = 'https://lynk.id/hyuuraofc'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ MEDIA SOSSIAL ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.sig = 'https://instagram.com/F'
-global.sgh = 'https://github.com/F' //github
+global.sig = 'https://instagram.com/_b4c00t4an_s3l3b'
+global.sgh = 'https://github.com/hyuura-official' //github
 global.sgc = 'https://chat.whatsapp.com/LmbsOrpSGEpDdFdpuukmmP' //group whatsapp
 global.sdc = '-' //discord
-global.snh = 'https://youtu.be/gWaL-UNxbXo'
+global.snh = 'https://youtube.com/channel/UCjoPsysjCn2Qa0dRalUb2mg'
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ DONASI ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.pdana = '0838 6673 7879'
+global.pdana = '0813 6128 1833'
 global.povo = '-'
-global.pgopay = '0838 6673 7879'
+global.pgopay = '0813 6128 1833'
 global.plinkaja = '-' //link aja
-global.ppulsa = '0838 6673 7879'
-global.ppulsa2 = '083866737879'
+global.ppulsa = '0813 6128 1833'
+global.ppulsa2 = '083838186170'
 global.psaweria = 'https://saweria.co/hyuura'
 
 
@@ -343,9 +343,9 @@ global.hoppai = ['https://telegra.ph/file/f34d76df4a2065af1a5ba.jpg','https://te
 
 
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/F"
+global.urlnya = "https://www.instagram.com/hyuura-official"
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '0838 6673 7879'
+global.phn = '0813 6128 1833'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ JNGN DI UBAH ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
